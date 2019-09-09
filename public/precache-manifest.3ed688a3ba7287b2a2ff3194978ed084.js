977f372cbf56dabfd6f9b52eedfb54f0a51b731d
Vadim Veksler <reyzele91@gmail.com>
2019-09-09 16:43:49 +0300

    server for order-taxi-app done

---
+++ public/precache-manifest.3ed688a3ba7287b2a2ff3194978ed084.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dcad51870e785186e72c5bca7b880ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4960cefe3715a93e8b9d",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "a26f93d0098f5b6a2e76",
+    "url": "/static/css/main.458f423a.chunk.css"
+  },
+  {
+    "revision": "4960cefe3715a93e8b9d",
+    "url": "/static/js/2.c64feb9c.chunk.js"
+  },
+  {
+    "revision": "a26f93d0098f5b6a2e76",
+    "url": "/static/js/main.ef00da5f.chunk.js"
+  },
+  {
+    "revision": "84727742f0df45b9c695",
+    "url": "/static/js/runtime~main.42275768.js"
+  }
+]);
